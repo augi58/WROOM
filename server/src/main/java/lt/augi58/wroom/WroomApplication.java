d8758179a3523c7e71121f92f1c29da21a94a327
augi58 <56911627+augi58@users.noreply.github.com>
2021-04-12 13:52:13 +0300

    initialize project

---
+++ server/src/main/java/lt/augi58/wroom/WroomApplication.java
@@ -0,0 +1,13 @@
+package lt.augi58.wroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WroomApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/lt/augi58/wroom/WroomApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.augi58.wroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
